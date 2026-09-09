9cdda4bdec20efa99e9738b55063da6d1a8c56d1
Lucas Williams <lucasnwilliams2008@gmail.com>
2026-09-09 14:01:47 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Lucas Williams
+* Course: CSCI 1250, Section 002
+* Assignment: Lab 0, Hello World
+* Date: September 9, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Lucas Williams.");
+Console.WriteLine("This is my first program at ETSU.");
